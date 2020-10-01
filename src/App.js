87b684662b5aa6cefccd5926b7258d7e23aeabe7
miguel <mr.almiguel@gmail.com>
2020-10-01 09:24:46 +0200

    first update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import NavBar from './components/Navbar'
+import Footer from './components/footer';
+
 
 function App () {
   return (
+    <>
     <NavBar/>
+    <Footer/>
+    </>
   )
 }
 
---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => 
+<div className="doc-footer"> 
+    <p> Global verification is a trademark of <a href="https://www.ejctechnology.co.za/" target="_blank"
+            rel="noopener noreferrer" >EJC Technology</a> Copyright &copy; 2020 ejc technology. All rights reserved.
+             </p>
+</div>
+
+
+export default Footer;

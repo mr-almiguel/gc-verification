b4c82b5ab6bd31b6e11ee32243aee0c23dd2c77c
miguel <mr.almiguel@gmail.com>
2020-10-01 08:46:34 +0200

    first step to start building the application

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../App.css'
+
+
+
+function NavBar () {
+    return (
+        <> 
+        <div className="headin"></div>
+        <div className="top-heading">
+            <h1>Weclome to <a href="https://www.ejctechnology.co.za/" target="_blank"
+            rel="noopener noreferrer" >EJC Technology</a> </h1> 
+        </div>
+        <div className="body-content">
+            <p>Our journey starts here</p>
+        </div>
+        </>
+    )
+}
+
+
+export default NavBar;
